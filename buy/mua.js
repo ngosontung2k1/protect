9a9acdfc726d60b2c0cb647bca83dbf8688d441f
Ngo Son Tung <ngosontung0309@gmail.com>
2021-05-22 20:31:05 +0700

    project 45%

--- buy/mua.js
+++ buy/mua.js
@@ -1,18 +1,18 @@
 document.addEventListener("DOMContentLoaded", function(){
     console.log('Loaded page')
     window.addEventListener('scroll',function(){
-        if(window.pageYOffset<300){
+        if(window.pageYOffset<250){
             console.log('dang o vi tri : '),window.pageYOffset;
         }
-        if(window.pageYOffset>300)
-        console.log(' dang o vi tri lon hon 300');
+        if(window.pageYOffset>250)
+        console.log(' dang o vi tri lon hon 250');
     })
 })
 
 document.addEventListener("DOMContentLoaded", function(){
     var top = document.querySelector('.top');
     window.addEventListener('scroll',function(){
-        if(window.pageYOffset>300){
+        if(window.pageYOffset>250){
             top.classList.add('topnho');
         }else{
             top.classList.remove("topnho");
